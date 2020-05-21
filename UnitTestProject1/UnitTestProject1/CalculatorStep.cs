f13e1042b092c80e79eb724f006690458d66b703
Weldes <weldes.silva@escolarmanager.com.br>
2020-05-21 12:57:19 -0300

    adicionado

---
+++ UnitTestProject1/UnitTestProject1/CalculatorStep.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace UnitTestProject1
+{
+    [Binding]
+    public class CalculatorStep
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            ScenarioContext.StepIsPending();
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            ScenarioContext.StepIsPending();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            ScenarioContext.StepIsPending();
+        }
+    }
+}
